f898d6f47e19d0a92203649ef6bb85a1d362db5a
Jaapio <jaap@ijaap.nl>
2021-02-25 20:42:29 +0100

    WIP

--- src/phpDocumentor/Descriptor/ApiSetDescriptor.php
+++ src/phpDocumentor/Descriptor/ApiSetDescriptor.php
@@ -18,16 +18,34 @@ use phpDocumentor\Configuration\Source;
 
 final class ApiSetDescriptor extends DocumentationSetDescriptor
 {
+    /** @var Collection<FileDescriptor> */
+    private $files;
+
+    /** @var Collection<NamespaceDescriptor> */
+    private $namespaces;
+
     /** @var ApiSpecification */
     private $apiSpecification;
 
-    public function __construct(string $name, Source $source, string $output, ApiSpecification $apiSpecification)
+    public function __construct(string $name, Source $source, string $output, Collection $files, Collection $namespaces, ApiSpecification $apiSpecification)
     {
         parent::__construct();
         $this->name = $name;
         $this->source = $source;
         $this->output = $output;
+        $this->files = $files;
+        $this->namespaces = $namespaces;
         $this->apiSpecification = $apiSpecification;
+    }
+
+    public function getFiles(): Collection
+    {
+        return $this->files;
+    }
+
+    public function getNamespaces(): Collection
+    {
+        return $this->namespaces;
     }
 
     public function getSettings(): ApiSpecification
